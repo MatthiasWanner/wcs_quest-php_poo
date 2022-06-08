58ecfbd1eb3cb34606be3c289aaccdb1aae276a2
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-08 14:02:39 +0200

    :ok_hand: (challenge) Throw exception when start car with park brake.

--- public/index.php
+++ public/index.php
@@ -1,74 +1,37 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Quest\Bicycle;
-use App\Quest\SkateBoard;
 use App\Challenge\Car;
-use App\Challenge\Truck;
-use App\Challenge\MotorWay;
-use App\Challenge\PedestrianWay;
-use App\Challenge\ResidentialWay;
-
-$firstBicycle = new Bicycle('red');
-$secondBicycle = new Bicycle('blue');
 
 $firstChallengeCar = new Car('blue', 5, 'electric');
+$firstChallengeCar->setParkBrake(true);
+
 $secondChallengeCar = new Car('red', 4, 'gasoline');
+$secondChallengeCar->setParkBrake(false);
 
-$firstTruck = new Truck('white', 3, 'gasoil', 10000);
-$secondTruck = new Truck('black', 3, 'gasoil', 15000);
-
-$skateBoard = new SkateBoard();
-
-$vehicles = [$firstBicycle, $secondBicycle, $firstChallengeCar, $secondChallengeCar, $firstTruck, $secondTruck, $skateBoard];
-
-$motorWay = new MotorWay();
-$motorWay->setCurrentVehicules($vehicles);
-
-$pedestrianWay = new PedestrianWay();
-$pedestrianWay->setCurrentVehicules($vehicles);
-
-$residentialWay = new ResidentialWay();
-$residentialWay->setCurrentVehicules($vehicles);
-
-function printVehicle($vehicle)
+function startCar(Car $car)
 {
-    $vehicleColor = $vehicle->getColor();
-    switch ($vehicle) {
-        case $vehicle instanceof Bicycle:
-            return $vehicleColor . ' bicycle';
-            break;
-        case $vehicle instanceof Car:
-            return $vehicleColor . ' car';
-            break;
-        case $vehicle instanceof Truck:
-            return $vehicleColor . ' truck';
-            break;
-        case $vehicle instanceof SkateBoard:
-            return 'skate board';
-            break;
+    try {
+        return $car->start();
+    } catch (\Exception $e) {
+        return $e->getMessage();
     }
+}
+
+function printParkBrakeSate(Car $car)
+{
+    return $car->getParkBrakeState() ? 'engaged' : 'unlocked';
 }
 
 ?>
 
-<p>👇 This is a list of current vehicules on a motor way: </p>
-<ul>
-    <?php foreach ($motorWay->getCurrentVehicles() as $motorVehicule) {
-        echo '<li>' . printVehicle($motorVehicule) . '</li>';
-    } ?>
-</ul>
+<p>
+    🚗 I try to start the car #1 with the park brake: <br />
+    Park brake status: <?php echo printParkBrakeSate($firstChallengeCar) ?><br />
+    Result: <?php echo startCar($firstChallengeCar) ?>
+</p>
 
-<p>👇 This is a list of current vehicules on a pedestrian way: </p>
-<ul>
-    <?php foreach ($pedestrianWay->getCurrentVehicles() as $pedestrianVehicule) {
-        echo '<li>' . printVehicle($pedestrianVehicule) . '</li>';
-    } ?>
-</ul>
-
-<p>👇 This is a list of current vehicules on a residential way: </p>
-<ul>
-    <?php foreach ($residentialWay->getCurrentVehicles() as $residentialVehicule) {
-        echo '<li>' . printVehicle($residentialVehicule) . '</li>';
-    } ?>
-</ul>
+<p>🚗 I try to start the car #2 without the park brake: <br />
+    Park brake status: <?php echo printParkBrakeSate($secondChallengeCar) ?><br />
+    Result: <?php echo startCar($secondChallengeCar) ?>
+</p>
--- src/Challenge/Car.php
+++ src/Challenge/Car.php
@@ -6,13 +6,34 @@ use App\Challenge\RunningEnergyVehicle;
 
 class Car extends RunningEnergyVehicle
 {
+    private bool $hasParkBrake = true;
+
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats, $energy, 4);
     }
 
+    public function getParkBrakeState(): bool
+    {
+        return $this->hasParkBrake;
+    }
+
+    public function setParkBrake(bool $hasParkBrake): void
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+
     public function introduceVehicle(): string
     {
         return parent::introduceProperties('car');
+    }
+
+    public function start(): string
+    {
+        if ($this->hasParkBrake) {
+            throw new \Exception('The car have the park brake!');
+        }
+        $this->setCurrentSpeed(15);
+        return "Start !";
     }
 }
